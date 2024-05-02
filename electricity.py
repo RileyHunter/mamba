e98740e330b22ef225e5a0daf41f34d805a77ecd
= <postagestamp@windowslive.com>
2024-05-03 10:53:54 +1200

    add electricity

---
+++ electricity.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def get_data():
+    data = np.loadtxt('reads_2400.txt')
+
+    # Unique characters
+    vals = sorted(list(set(data)))
+    print(','.join(str(v) for v in vals))
+    vocab_size = len(vals)
+    print(vocab_size)
+
+    #Tokenizers
+    encode = lambda xx: xx
+    decode = lambda xx: ','.join(str(x) for x in xx)
+    return data, encode, decode, vocab_size
