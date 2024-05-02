0ae84752505bfaaba109d0c06392b2a34f4d69e6
= <postagestamp@windowslive.com>
2024-05-03 11:27:18 +1200

    .

--- electricity.py
+++ electricity.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def get_data():
-    data = np.loadtxt('reads_81888.txt')
+    data = [str(v) for v in np.loadtxt('reads_81888.txt')]
 
     # Unique characters
     vals = sorted(list(set(data)))
-    print(','.join(str(v) for v in vals))
+    print(','.join(vals))
     vocab_size = len(vals)
     print(vocab_size)
 
