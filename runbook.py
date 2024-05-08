dabbeded3a1710ee0e5623873f3c8eb2c8afbfc6
= <postagestamp@windowslive.com>
2024-05-08 14:34:36 +1200

    work

--- runbook.py
+++ runbook.py
@@ -3,22 +3,14 @@ import torch.nn as nn
 from torch.nn import  functional as F
 from torch.nn.parameter import Parameter
 from tqdm import tqdm
-from mamba_ssm import Mamba
 from electricity import get_data
+from bigramnn import BigramNeuralNetwork, batch_size, block_size, device
 #hyperparams
-#epochs = 10
 lr = 1e-3
-batch_size = 64
-block_size = 256
-device = "cuda" if torch.cuda.is_available() else "cpu"
 max_iters = 1000 # Default: 10000
 print_iters = 100
 eval_iters = 10
 eval_interval = 300
-n_embed=384
-n_heads = 6
-n_layers = 6
-dropout = 0.2
 # ---------
 
 raw, encode, decode, vocab_size = get_data()
@@ -54,141 +46,6 @@ def estimate_loss():
     out[split] = losses.mean()
   model.train()
   return out
-
-
-class SelfAttentionHead(nn.Module):
-  def __init__(self, head_size):
-    super().__init__()
-    self.keys = nn.Linear(n_embed, head_size)
-    self.queries = nn.Linear(n_embed, head_size)
-    self.values = nn.Linear(n_embed, head_size)
-    self.head_size = head_size
-    self.n_embed = n_embed
-    self.register_buffer('tril', torch.tril(torch.ones((block_size,block_size))).to(device))
-    self.dropout = nn.Dropout(dropout)
-
-  def forward(self, x):
-    B,T,C = x.shape
-    k = self.keys(x) # (B,T,C_h)
-    q = self.queries(x) # (B,T,C_h)
-    v = self.values(x) # (B,T,C_h)
-    wei = k @ q.transpose(-1,-2) * C**(-0.5)# (B,T,T)
-    wei = wei.masked_fill( self.tril[:T,:T]==0, float('-inf'))
-    # wei = F.softmax(wei, dim=-1) # (B,T,T)
-    wei = torch.log(torch.exp(wei)+1) # (B,T,T)
-    wei = self.dropout(wei)
-    out = wei @ v # (B,T,C_h)
-    return out
-
-
-class LayerNorm(nn.Module):
-  def __init__(self, dim) -> None:
-    super().__init__()
-    self.eps = 1e-5
-    # params
-    self.gamma = nn.Parameter(torch.ones(dim))
-    self.beta = nn.Parameter(torch.zeros(dim))
-
-  def forward(self, x):
-    xmean = x.mean(dim=1, keepdim=True)
-    xvar = ((x-xmean)**2).mean(dim=1, keepdim=True)
-    xhat = (x-xmean) / torch.sqrt(xvar + self.eps)
-    self.out = self.gamma * xhat + self.beta
-    return self.out
-
-  def parameters(self):
-    return [self.gamma, self.beta]
-
-class MultiHeadAttention(nn.Module):
-  def __init__(self, n_heads, head_size) -> None:
-    super().__init__()
-    self.heads = nn.ModuleList([SelfAttentionHead(head_size) for _ in range(n_heads)])
-    self.proj = nn.Linear(n_embed, n_embed)
-    self.dropout = nn.Dropout(dropout)
-
-  def forward(self, x):
-    B,T,C = x.shape
-    out = torch.cat([head(x) for head in self.heads], dim=-1)
-    out = self.proj(out)
-    out = self.dropout(out)
-    return out
-
-class FeedForward(nn.Module):
-  def __init__(self, n_embed) -> None:
-    super().__init__()
-    self.ffn = nn.Sequential(
-      nn.Linear(n_embed, 4*n_embed),
-      nn.ReLU(),
-      nn.Linear(4*n_embed, n_embed),
-      nn.Dropout(dropout),
-    )
-  def forward(self, x):
-    return self.ffn(x)
-
-class Block(nn.Module):
-  def __init__(self, n_embed, n_heads) -> None:
-    super().__init__()
-    self.head_size = n_embed // n_heads
-    # self.sa_head = MultiHeadAttention(n_heads, self.head_size)
-    self.sa_head = Mamba(
-      # This module uses roughly 3 * expand * d_model^2 parameters
-      d_model=n_embed, # Model dimension d_model
-      d_state=16,  # SSM state expansion factor
-      d_conv=4,    # Local convolution width
-      expand=2,    # Block expansion factor
-  ).to("cuda")
-    self.ffn = FeedForward(n_embed)
-    self.ln1 = nn.LayerNorm(n_embed)
-    self.ln2 = nn.LayerNorm(n_embed)
-
-
-  def forward(self, x):
-    x = x + self.sa_head(self.ln1(x))
-    x = x + self.ffn(self.ln2(x))
-
-    return x
-
-class BigramNeuralNetwork(nn.Module):
-  def __init__(self,vocab_size):
-    super().__init__()
-    self.token_embedding_table = nn.Embedding(vocab_size,n_embed)
-    self.position_embedding_table = nn.Embedding(block_size,n_embed)
-    self.sa_head = MultiHeadAttention(4,int(n_embed/4))
-    self.lm_head = nn.Linear(n_embed,vocab_size)
-    self.ffn = FeedForward(n_embed)
-    self.blocks = nn.Sequential(*[Block(n_embed,n_heads=n_heads) for _ in range(n_layers)])
-
-
-  def forward(self, idx, targets=None):
-    # idx = idx[:,-block_size:]
-    B,T = idx.shape
-    tok_emb = self.token_embedding_table(idx) # (B,T,C_e)
-    pos_emb = self.position_embedding_table(torch.arange(T,device=device)) # (T,C_e)
-    x = tok_emb + pos_emb # (B,T,C_e)
-    x = self.blocks(x) # (B,T,C_e)
-    logits = self.lm_head(x) # (B,T,vocab_size)
-    if targets is None:
-      loss = None
-    else:
-      B,T,C = logits.shape
-      logits = logits.view(B*T,C)
-      targets = targets.view(B*T)
-      loss = F.cross_entropy(logits, targets)
-      logits = logits.view(B,T,C)
-    return logits, loss
-  def generate(self, idx, max_new_tokens):
-    # idx is (B,T)
-    idx_next = []
-    for i in range(max_new_tokens):
-      idx_cond = idx[:,-block_size:]
-      logits, loss = self(idx_cond)
-      last_timestep = logits[:,-1,:]
-      probs = F.softmax(last_timestep, dim=1)
-      next_index = torch.multinomial(probs, num_samples=1)
-      idx = torch.cat((idx, next_index), dim=1)
-    for arr in idx:
-      print(decode(arr.cpu().detach().numpy()))
-    return idx
 
 model = BigramNeuralNetwork(vocab_size)
 optimizer = torch.optim.AdamW(model.parameters(),lr=lr)
@@ -232,8 +89,9 @@ for iter in tqdm(range(epoch ,max_iters)):
     model.eval()
     with torch.no_grad():
       #Generate from the model:
-      output = m.generate(torch.zeros((1,2), dtype=torch.long).to(device).contiguous(), 1000  )[0].tolist()
-
+      output = m.generate(torch.zeros((1,2), dtype=torch.long).to(device).contiguous(), 1000  )
+      for arr in output:
+        print(decode(arr.cpu().detach().numpy()))
     print(f"Step {iter}, train loss:{losses['train']:.4f}, test loss:{losses['test']:.4f}")
     model.train()
 
@@ -248,4 +106,6 @@ for iter in tqdm(range(epoch ,max_iters)):
   optimizer.step()
 torch.save(model.state_dict(), "./differentattention/model.pt")
 #Generate from the model:
-output = m.generate(torch.zeros((1,2), dtype=torch.long).to(device),1000)[0].tolist()
+output = m.generate(torch.zeros((1,2), dtype=torch.long).to(device).contiguous(), 1000  )
+for arr in output:
+    print(decode(arr.cpu().detach().numpy()))
