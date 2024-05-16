93f3e376c858dae79e47dc9560da4308f1790b5c
= <postagestamp@windowslive.com>
2024-05-16 16:54:17 +1200

    .

--- runbook.py
+++ runbook.py
@@ -155,9 +155,9 @@ output_raw = []
 for i in range(pred_size):
   preds = m.generate(torch.stack([prefix[i:i+block_size]]).to(device), 1)
   decoded = decode(preds[0].cpu().detach().numpy())
-  print('decode', decoded)
+  print('decode', decoded, decoded[-1])
   output_raw.append(decoded[-1])
-output = np.append(val_data[:block_size].cpu().detach().numpy(), output_raw)
+output = np.append(decode(val_data[:block_size].cpu().detach().numpy()), output_raw)
 print('Preds')
 print(output)
 print('Ground truth')
